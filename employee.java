32fde94de9292c869488b2f2d2506af1879adf64
Versha Krishnani <versha.krishnani@atcs.com>
2022-04-27 15:30:30 +0530

    new file added

---
+++ employee.java
@@ -0,0 +1,15 @@
+package com.stack;
+
+public class employee {
+
+	public employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
